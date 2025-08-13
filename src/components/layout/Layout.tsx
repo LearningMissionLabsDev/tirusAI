ba647fb57f14a79888d5271502ede5e33d08cb7e
LearningMissionLabsDev <rouben@learningmissionlabs.com>
2025-08-13 15:02:06 -0700

    add background image

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,9 @@
-
 import React from 'react';
 import type { ReactNode } from 'react';
 import { Box } from '@mui/material';
 import Header from '../ui/Header';
 import Footer from '../ui/Footer/Footer';
+import TirusBG from '/assets/TirusBG.svg'
 
 interface LayoutProps {
   children: ReactNode;
@@ -13,12 +13,19 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Box
       sx={{
-        backgroundColor: '#0A0F1C',
         minHeight: '100vh',
         display: 'flex',
         flexDirection: 'column',
       }}
     >
+        <Box sx={{
+        backgroundImage: `url(${TirusBG})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%',
+        zIndex: 1
+      }}>
       <Header />
       <Box
         component="main"
@@ -30,7 +37,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         {children}
       </Box>
       <Footer/>
-    </Box>
+    </Box></Box>
   );
 };
 
