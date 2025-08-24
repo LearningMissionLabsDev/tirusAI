6deabf6ec11ca76afdf8f0253ced6d8d5c7ff592
LearningMissionLabsDev <rouben@learningmissionlabs.com>
2025-08-24 16:18:34 -0700

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/layout/Layout'
-import Index from './pages/landing/Index'
+import Landing from './pages/landing/Landing'
+import AboutUs from './pages/aboutUs/AboutUs'
 
 function App() {
 
   return (
     <>
-    <Layout>
-      <Index/>
-    </Layout>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+        <Route element={<Layout />}>
+          <Route path="/about" element={<AboutUs />} />
+        </Route>
+      </Routes>
     </>
   )
 }
