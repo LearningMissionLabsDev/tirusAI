a93dd4171237a58a905b1d4ce261e2cdae58e0b1
LearningMissionLabsDev <rouben@learningmissionlabs.com>
2025-08-25 00:37:08 -0700

    remove comments

--- src/pages/landing/ContactUs.tsx
+++ src/pages/landing/ContactUs.tsx
@@ -1,95 +1,3 @@
-// import React from "react";
-// import { Box, Container, Typography, useTheme } from "@mui/material";
-// import ContactCard from "../../components/ui/Cards/ContactCard";
-// import ContactForm from "../../components/form/ContactForm";
-// import contactCardData from "../../data/contactCardData.json";
-// import contactFormData from "../../data/contactFormData.json";
-
-// type ContactCardItem = {
-//   icon: string;
-//   title: string;
-//   description: string;
-//   link: string;
-// };
-
-// const ContactUs: React.FC = () => {
-//   const theme = useTheme();
-
-//   return (
-//     <Box maxWidth="100%" sx={{ position: "relative", justifyContent: "center", alignItems: "center", px: "16px", mx: "auto", overflow: "hidden" }}>
-//       <Box>
-//         <Typography
-//           sx={{
-//             ...theme.typography.h1,
-//             fontFamily: "Poppins, sans-serif",
-//             fontWeight: 600,
-//             color: "#FFFFFFE6",
-//             textAlign: "center",
-//             mb: 6,
-//           }}
-//         >
-//           {contactCardData.sectionTitle}
-//         </Typography>
-//       </Box>
-
-//       <Container maxWidth={false} disableGutters sx={{ p: "0 !important", m: "0 !important"  }}>
-//         {/* Center the whole row and cap its width */}
-// <Box
-//   sx={{
-//     display: "flex",
-//     flexDirection: { xs: "column", md: "row" },
-//     gap: { xs: 3, sm: 3, md: 3 },
-//     alignItems:  "center", // vertical centering on md+
-//     justifyContent: "center",
-//     width: "100%",
-//     maxWidth: 1200,           // <= tune this (e.g., 1100–1280)
-//     mx: "auto",               // <= centers the block
-//   }}
-// >
-//           {/* Left: Contact info (no flex:1) */}
-// <Box
-//   sx={{
-//     display: "flex",
-//     flexDirection: "column",
-//     gap: 3,
-//     width: { xs: "100%", sm: "100%", md: 384 }, // fixed width on desktop
-//   }}
-// >
-//             {contactCardData.items.map((item: ContactCardItem, idx: number) => (
-//               <ContactCard
-//                 key={idx}
-//                 icon={item.icon}
-//                 title={item.title}
-//                 description={item.description}
-//                 link={item.link}
-//               />
-//             ))}
-//           </Box>
-
-// <Box
-//   sx={{
-//     width: "100%",
-//     borderRadius: "12px",
-//     height: {md: "291px"},
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   }}
-// >
-//             <ContactForm
-//               title={contactFormData.title}
-//               fields={contactFormData.fields}
-//               submitText={contactFormData.submitButton.text}
-//             />
-//           </Box>
-//         </Box>
-//       </Container>
-//     </Box>
-//   );
-// };
-
-// export default ContactUs;
-
 import React from "react";
 import { Box, Container, Typography, useTheme } from "@mui/material";
 import ContactCard from "../../components/ui/Cards/ContactCard";
@@ -144,8 +52,8 @@ const ContactUs: React.FC = () => {
             sx={{
               display: "flex",
               flexDirection: "column",
-              gap: {xs: 2.5, sm: 3, md: 3},
-              width: { xs: "99.54%", sm: "99.54%", md: 384 }, // fixed width on desktop
+              gap: { xs: 2.5, sm: 3, md: 3 },
+              width: { xs: "99.54%", sm: "99.54%", md: 384 },
             }}
           >
             {contactCardData.items.map((item: ContactCardItem, idx: number) => (
@@ -172,6 +80,7 @@ const ContactUs: React.FC = () => {
             <ContactForm
               title={contactFormData.title}
               fields={contactFormData.fields}
+              messages={contactFormData.messages}
               submitText={contactFormData.submitButton.text}
             />
           </Box>
